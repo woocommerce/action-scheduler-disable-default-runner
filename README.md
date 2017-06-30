b53be61be5685cdbcb81d53d46ca3966e8537e9b
Brent Shepherd <thenbrent@gmail.com>
2017-06-30 11:42:37 -0700

    Initial commit of mini-extension boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# prospress-mini-extension-boilerplate
-Create mini-extensions to help customers customise functionality with Subscriptions, One Page Checkout, and perhaps even WooCommerce in general.
+# {plugin_name}
+
+{plugin_short_description}
+
+## Installation
+
+To install:
+
+1. Download the latest version of the plugin [here](https://github.com/Prospress/{plugin_slug}/archive/master.zip)
+1. Go to **Plugins > Add New > Upload** administration screen on your WordPress site
+1. Select the ZIP file you just downloaded
+1. Click **Install Now**
+1. Click **Activate**
+
+### Updates
+
+To keep the plugin up-to-date, use the [GitHub Updater](https://github.com/afragen/github-updater).
+
+## Reporting Issues
+
+If you find an problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/{plugin_slug}/issues/new).
+
+---
+
+<p align="center">
+	<a href="https://prospress.com/">
+		<img src="https://cloud.githubusercontent.com/assets/235523/11986380/bb6a0958-a983-11e5-8e9b-b9781d37c64a.png" width="160">
+	</a>
+</p>
---
+++ mini-extension-boilerplate.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Plugin Name: {plugin_name}
+ * Plugin URI: https://github.com/Prospress/{plugin_slug}/
+ * Description: {plugin_short_description}
+ * Author: Prospress Inc.
+ * Author URI: https://prospress.com/
+ * License: GPLv3
+ * Version: 1.0.0
+ * Version: 1.0.0
+ * Requires at least: 4.0
+ * Tested up to: 4.8
+ *
+ * GitHub Plugin URI: Prospress/{plugin_slug}
+ * GitHub Branch: master
+ *
+ * Copyright 2017 Prospress, Inc.  (email : freedoms@prospress.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		{plugin_name}
+ * @author		Prospress Inc.
+ * @since		1.0
+ */
+
